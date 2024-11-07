6a7bad95615c675b190d6762ae54e719f3e864e0
Lydia <lydia@MacBook-Pro-de-Angel.local>
2024-11-07 18:10:25 +0100

    Task 4: Math tests

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,45 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import get_random_number, get_random_operation, math_generator
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
-        # Test if random numbers generated are within the specified range
+    def test_get_random_number(self):
+        """
+        Test if random numbers generated are within the specified range.
+        Return (bool): True or False.
+        """
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = get_random_number(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_get_random_operation(self):
+        """
+        Test if random math operations are addition, substraction and multiplication only.
+        Return (bool): True or False.
+        """
+        op_list = ['+', '-', '*']
+        for _ in range(10):  # Test ten times.
+            rand_op = get_random_operation()
+            self.assertTrue(rand_op in op_list)
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+    def test_math_generator(self):
+        """
+        Test if math operations are executed correctly.
+        Return (bool): True or False.
+        """
+        test_cases = [
+            (5, 2, '+', '5 + 2', 7),
+            (5, 2, '-', '5 - 2', 3),
+            (5,  2, '*', '5 * 2', 10),
+            (3, 3, '+', '3 + 3', 6)
+        ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        for num1, num2, operator, expected_problem, expected_answer in test_cases:
+            mathgen_answer = math_generator(num1, num2, operator)
+            self.assertTrue(mathgen_answer == expected_answer)
 
 if __name__ == "__main__":
     unittest.main()
